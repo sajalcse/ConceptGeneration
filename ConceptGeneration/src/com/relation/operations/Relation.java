2227e21a8f4405b83ad308e7acad86f05a9bf021
sajalcse <sam@01836>
2019-06-21 10:30:24 -0400

    Add Methods in utility class and relation class

--- ConceptGeneration/src/com/relation/operations/Relation.java
+++ ConceptGeneration/src/com/relation/operations/Relation.java
@@ -1,194 +1,530 @@
 package com.relation.operations;
 
+import com.relation.lattice.Lattice;
+import com.relation.utility.Utility;
+
 public class Relation {
+
+	/**
+	 * This is the number of row in the relation.
+	 */
 	private int row;
+	/**
+	 * This is the number of column in the relation.
+	 */
 	private int column;
-	private boolean matrix[][];
+	/**
+	 * This two dimensional array represents the relation.
+	 */
+	private int matrix[][];
+	/**
+	 * This field represents the type of lattice.
+	 */
+	private static Lattice lattice;
 
-	public Relation(int row, int column) {
-		this.setRow(row);
-		this.setColumn(column);
-		setMatrix(new boolean[this.row][this.column]);
-	}
-
+	/**
+	 * This is the default constructor.
+	 */
 	public Relation() {
 		this.row = 0;
 		this.column = 0;
 	}
 
+	/**
+	 * This is the constructor which takes initial parameter to define the
+	 * relation.
+	 * 
+	 * @param row
+	 *            the number of row in relation
+	 * @param column
+	 *            the number of column in relation
+	 * @param lattice
+	 *            the type of lattice.
+	 */
+	public Relation(int row, int column, Lattice lattice) {
+		this.row = row;
+		this.column = column;
+		this.lattice = lattice;
+		setMatrix(new int[row][column]);
+	}
+
+	/**
+	 * This method accesses the private field in row.
+	 * 
+	 * @return the row number.
+	 */
 	public int getRow() {
 		return row;
 	}
 
+	/**
+	 * This method sets the row in the relation.
+	 * 
+	 * @param row
+	 *            the row number.
+	 */
 	public void setRow(int row) {
 		this.row = row;
 	}
 
+	/**
+	 * This method accesses the private field in column.
+	 * 
+	 * @return the column number.
+	 */
 	public int getColumn() {
 		return column;
 	}
 
+	/**
+	 * This method sets the column in the relation.
+	 * 
+	 * @param column
+	 *            the column number.
+	 */
 	public void setColumn(int column) {
 		this.column = column;
 	}
 
-	public boolean[][] getMatrix() {
+	/**
+	 * This methods accesses the private field in matrix.
+	 * 
+	 * @return the attached matrix with a relation.
+	 */
+	public int[][] getMatrix() {
 		return matrix;
 	}
 
-	public void setMatrix(boolean matrix[][]) {
+	/**
+	 * This method sets the matrix in the relation.
+	 * 
+	 * @param matrix
+	 *            the matrix to attach with the relation.
+	 */
+	public void setMatrix(int matrix[][]) {
 		this.matrix = matrix;
 	}
 
-	public Relation Union(Relation r1) {
-		if ((this.getRow() != r1.getRow())
-				&& (this.getColumn() != r1.getColumn())) {
-			System.out.println("Matrix are incompatiable");
+	/**
+	 * This method determines the union of two relations with same dimension.
+	 * Every element of one relation is compared with the corresponding element
+	 * from the other relation and if one of them is true then it writes true in
+	 * the output relation.
+	 * 
+	 * @param relation
+	 *            is one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return union of two relation.
+	 */
+	public Relation Union(Relation relation) {
+		if ((this.getRow() != relation.getRow())
+				&& (this.getColumn() != relation.getColumn())) {
+			System.err
+					.println("Union Operation is not possible because number of column is different!");
 			return null;
 		} else {
-			Relation resultRelation = new Relation(r1.getRow(), r1.getColumn());
-			boolean resultMatrix[][] = null;
+			Relation resultRelation = new Relation(relation.getRow(),
+					relation.getColumn(), lattice);
+			int resultMatrix[][] = null;
 			resultMatrix = resultRelation.getMatrix();
 			for (int i = 0; i < row; i++) {
 				for (int j = 0; j < column; j++) {
-					resultMatrix[i][j] = (this.getMatrix()[i][j])
-							|| (r1.getMatrix()[i][j]);
+					resultMatrix[i][j] = lattice.Join(this.getMatrix()[i][j],
+							relation.getMatrix()[i][j]);
 				}
 			}
 			resultRelation.setMatrix(resultMatrix);
+			System.out.println("---Union Start----");
+			Utility.PrintArray(resultRelation.getMatrix());
 			return resultRelation;
 		}
 
 	}
 
-	public Relation Intersection(Relation r1) {
-		if ((this.getRow() != r1.getRow())
-				&& (this.getColumn() != r1.getColumn())) {
-			System.out.println("Matrix are incompatiable");
+	/**
+	 * This method determines the intersection of two relation with same
+	 * dimension. Every element of one relation is compared with the
+	 * corresponding element from the other relation and if both of them is true
+	 * then it writes true in the output relation.
+	 * 
+	 * @param relation
+	 *            is one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return intersection of two relation
+	 */
+	public Relation Intersection(Relation relation) {
+		if ((this.getRow() != relation.getRow())
+				&& (this.getColumn() != relation.getColumn())) {
+			System.err
+					.println("Intersection Operation is not possible because number of column is different!");
 			return null;
 		} else {
-			Relation resultRelation = new Relation(r1.getRow(), r1.getColumn());
-			boolean resultMatrix[][] = null;
+			Relation resultRelation = new Relation(relation.getRow(),
+					relation.getColumn(), lattice);
+			int resultMatrix[][] = null;
 			resultMatrix = resultRelation.getMatrix();
 			for (int i = 0; i < row; i++) {
 				for (int j = 0; j < column; j++) {
-					resultMatrix[i][j] = this.getMatrix()[i][j]
-							&& r1.getMatrix()[i][j];
+					resultMatrix[i][j] = lattice.Meet(this.getMatrix()[i][j],
+							relation.getMatrix()[i][j]);
 				}
 			}
 			resultRelation.setMatrix(resultMatrix);
+			System.out.println("---Intersection Start----");
+			Utility.PrintArray(resultRelation.getMatrix());
 			return resultRelation;
 		}
 	}
 
-	public Relation Composition(Relation r1) {
-		boolean resultMatrix[][] = null;
+	/**
+	 * This method calculates the relative multiplication of two relation. The
+	 * (x,z) element would be true in the resulting relation if and only if there
+	 * exists one element y such that (x,y) is true in the first relation and (y,z) is
+	 * true in the other relation.
+	 * 
+	 * @param is
+	 *            one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return the composition of two relation.
+	 */
+	public Relation Composition(Relation relation) {
+		int resultMatrix[][] = null;
 
-		if (this.getColumn() != r1.getRow()) {
-			System.out.println("A:Rows: " + this.getColumn()
-					+ " did not match B:Columns " + r1.getRow() + ".");
+		if (this.getColumn() != relation.getRow()) {
+			System.err
+					.println("First Relation column should be equal to the second relation row!");
 			return null;
 		} else {
 			Relation resultRelation = new Relation(this.getRow(),
-					r1.getColumn());
+					relation.getColumn(), lattice);
 			resultMatrix = resultRelation.getMatrix();
 
 			for (int v = 0; v < this.getColumn(); v++) {
 				for (int i = 0; i < this.getRow(); i++) {
-					for (int j = 0; j < r1.getColumn(); j++) {
-						resultMatrix[i][j] = resultMatrix[i][j]
-								|| (this.getMatrix()[i][v] && r1.getMatrix()[v][j]);
+					for (int j = 0; j < relation.getColumn(); j++) {
+						resultMatrix[i][j] = lattice.Join(
+								resultMatrix[i][j],
+								lattice.Meet(this.getMatrix()[i][v],
+										relation.getMatrix()[v][j]));
 					}
 				}
 
 			}
 			resultRelation.setMatrix(resultMatrix);
+			System.out.println("---Composition Start----");
+			Utility.PrintArray(resultRelation.getMatrix());
 			return resultRelation;
 		}
 	}
 
-	public boolean Implication(boolean p, boolean q) {
-		return !p || q;
-	}
-
-	public Relation LeftResidue(Relation r1) {
-		Relation resultRelation = new Relation(this.getColumn(), r1.getColumn());
-		int m = this.getRow();
+	/**
+	 * This method calculates the left residual of two relation R and S (R\S)
+	 * which is the greatest of all relation X with (R.X) is the subset of S.
+	 * 
+	 * @param relation
+	 *            one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return the left residue of two relation.
+	 */
+	public Relation RightResidue(Relation relation) {
+		Relation r = this;
+		Relation q = relation;
+		Relation resultRelation = new Relation(this.getColumn(),
+				relation.getColumn(), lattice);
+		int row = this.getRow();
 		for (int i = 0; i < resultRelation.getRow(); i++) {
 			for (int j = 0; j < resultRelation.getColumn(); j++) {
-				resultRelation.getMatrix()[i][j] = true;
-				for (int n = 0; n < m; n++) {
-					resultRelation.getMatrix()[i][j] = resultRelation
-							.getMatrix()[i][j]
-							&& Implication(this.getMatrix()[n][i],
-									r1.getMatrix()[n][j]);
+				resultRelation.getMatrix()[i][j] = lattice.GetOne();
+				for (int n = 0; n < row; n++) {
+					resultRelation.getMatrix()[i][j] = lattice.Meet(
+							resultRelation.getMatrix()[i][j], lattice
+									.Implication(this.getMatrix()[n][i],
+											relation.getMatrix()[n][j]));
 				}
 			}
 		}
+		System.out.println("---Right Start----");
+		Utility.PrintArray(resultRelation.getMatrix());
+		//System.out.println("---End Left Start----");
 		return resultRelation;
 	}
 
-	public Relation RightResidue(Relation r1) {
-		Relation resultRelation = new Relation(this.getRow(), r1.getRow());
-		int m = r1.getColumn();
+	/**
+	 * This method calculates the right residual of two relation R and S (R/S)
+	 * which is the greatest of all relation X with (X.R) is the subset of S.
+	 * 
+	 * @param relation
+	 *            one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return right residue of two relation.
+	 */
+	public Relation LeftResidue(Relation relation) {
+		Relation resultRelation = new Relation(this.getRow(),
+				relation.getRow(), lattice);
+		int column = relation.getColumn();
 		for (int i = 0; i < resultRelation.getRow(); i++) {
 			for (int j = 0; j < resultRelation.getColumn(); j++) {
-				resultRelation.getMatrix()[i][j] = true;
-				for (int n = 0; n < m; n++) {
-					resultRelation.getMatrix()[i][j] = resultRelation
-							.getMatrix()[i][j]
-							&& Implication(r1.getMatrix()[j][n],
-									this.getMatrix()[i][n]);
+				resultRelation.getMatrix()[i][j] = lattice.GetOne();
+				for (int n = 0; n < column; n++) {
+					resultRelation.getMatrix()[i][j] = lattice.Meet(
+							resultRelation.getMatrix()[i][j], lattice
+									.Implication(relation.getMatrix()[j][n],
+											this.getMatrix()[i][n]));
 				}
 			}
 		}
+		System.out.println("---Left Start----");
+		Utility.PrintArray(resultRelation.getMatrix());
 		return resultRelation;
 	}
 
-	public static Relation Epsilon(int m) {
-		int row = m;
-		int column = (int) Math.pow(2, row);
-		Relation resultRelation = new Relation(row, column);
+	/**
+	 * This method creates an entry for each subset. For example, there are a total of 8
+	 * subsets for 3 elements.Then this method creates a relation with dimension
+	 * 8 by 3 where row represents each of the subset and column represents each
+	 * object. Empty subset contain 0 for every column while subset with one
+	 * element contain 1 in the column which represents the element in the
+	 * subset.
+	 * 
+	 * @param element
+	 *            no of elements in the set.
+	 * @return the epsilon relation for the input.
+	 */
+	public static Relation Epsilon(int element) {
+		int row = element;
+		int column = (int) Math.pow(lattice.getNoOfElements(), row);
+		Relation resultRelation = new Relation(row, column, lattice);
 		for (int c = 0; c < column; c++) {
 			int n = c;
 			int r = row - 1;
 			while (n > 0) {
-				resultRelation.getMatrix()[r][c] = n % 2 == 1;
-				n = n / 2;
+				resultRelation.getMatrix()[r][c] = n
+						% lattice.getNoOfElements();
+				n = n / lattice.getNoOfElements();
 				r--;
 			}
 
 		}
+		System.out.println("---Epsilon Start----");
+		Utility.PrintArray(resultRelation.getMatrix());
 		return resultRelation;
 	}
 
-	public Relation SymetricQuotient(Relation r1) {
-		return this.LeftResidue(r1).Intersection(
-				this.Transpose().RightResidue(r1.Transpose()));
+	/**
+	 * This method calculates the intersection of left residue and right residue
+	 * to two relation R and S. Let define R:X->Y and S:X->Z. Then the symmetric
+	 * quotient relates an element y from Y to an element z from Z when y and z
+	 * have the same set of inverse image with respect to R and S respectively.
+	 * 
+	 * @param relation
+	 *            one of the input relation for union method. The other input
+	 *            relation accessed using this operator in java.
+	 * @return symmetric quotient of two relation.
+	 */
+	public Relation SymetricQuotient(Relation relation) {
+	
+		Relation right = this.RightResidue(relation);
+		Relation left = this.Transpose().LeftResidue(relation.Transpose());
+		return right.Intersection(left);
 	}
 
+	/**
+	 * This method derives all concepts from a relational data set.
+	 * 
+	 * @return it returns all concepts in the form of matrix where true
+	 *         represent concept.
+	 */
 	public Relation GenerateConcepts() {
-		return this.RightResidue(Epsilon(this.getRow()).LeftResidue(this))
+		Relation epsilon = Epsilon(this.getRow());
+		System.out.println("----epsilonss----");
+		Utility.PrintArray(epsilon.getMatrix());
+		
+		Relation epsilonRightRelation= epsilon.RightResidue(this);
+		System.out.println("----epsilonRightRelation----");
+		Utility.PrintArray(epsilonRightRelation.getMatrix());
+		
+		
+		Relation RelationLeftepsilonRightRelation = this.LeftResidue(epsilonRightRelation);
+		System.out.println("----RelationLeftepsilonRightRelation----");
+		Utility.PrintArray(RelationLeftepsilonRightRelation.getMatrix());
+		
+		
+
+		Relation symRelation = RelationLeftepsilonRightRelation.SymetricQuotient(epsilon);
+		System.out.println("----symRelation----");
+		Utility.PrintArray(symRelation.getMatrix());
+		
+		Relation identity = IdentityRelation(symRelation.getRow());
+		System.out.println("----identityyyy----");
+		Utility.PrintArray(identity.getMatrix());
+		
+		Relation finalmat = symRelation.Intersection(identity);
+		System.out.println("----finalmat----");
+		Utility.PrintArray(finalmat.getMatrix());
+		System.out.println("----");
+		return finalmat;
+		
+		/*return this.RightResidue(Epsilon(this.getRow()).LeftResidue(this))
 				.SymetricQuotient(Epsilon(this.getRow()))
-				.Intersection(IdentityRelation(this.getRow()));
+				.Intersection(IdentityRelation(this.getRow()));*/
 	}
 
-	public static Relation IdentityRelation(int m) {
-		int n = (int) Math.pow(2, m);
-		boolean[][] a = new boolean[n][n];
-		for (int i = 0; i < n; i++)
-			a[i][i] = true;
-		Relation resultRelation = new Relation(n, n);
-		resultRelation.setMatrix(a);
+	/**
+	 * This static method returns an identity relation i.e. a square relation
+	 * with ones on the main diagonal and zero elsewhere.
+	 * 
+	 * @param size
+	 *            the dimension of matrix. This size is set as the row and
+	 *            column of the identity relation.
+	 * @return the identity relation with specified dimension.
+	 */
+	public static Relation IdentityRelation(int size) {
+		int dimension = size;
+		int[][] matrix = new int[dimension][dimension];
+		for (int i = 0; i < dimension; i++)
+			matrix[i][i] = lattice.GetOne();
+		Relation resultRelation = new Relation(dimension, dimension, lattice);
+		resultRelation.setMatrix(matrix);
+		System.out.println("---Identity Start----");
+		Utility.PrintArray(resultRelation.getMatrix());
 		return resultRelation;
 	}
 
+	/**
+	 * This method flips the relation, that is it switches the row and column by
+	 * producing another relation.
+	 * 
+	 * @return it returns transposed relation
+	 */
 	public Relation Transpose() {
-		Relation r2 = new Relation(this.getColumn(), this.getRow());
+		Relation r2 = new Relation(this.getColumn(), this.getRow(), lattice);
 		for (int i = 0; i < this.getRow(); i++)
 			for (int j = 0; j < this.getColumn(); j++)
 				r2.getMatrix()[j][i] = this.getMatrix()[i][j];
 		return r2;
 	}
 
+	/**
+	 * This method create a relation with 1 and 0. The relation dimension
+	 * depends on the input parameters. For example, if m and n two parameters in
+	 * the function then relation consist of (m) rows and (m+n) columns. The result
+	 * relation contains 1 for those indexes where row and column represents same
+	 * element from m and 0 elsewhere.
+	 * 
+	 * @param m the first parameter.
+	 * @param n the second parameter.
+	 * @return the iota relation consist of 1 and 0.
+	 */
+	public static Relation Iota(int m, int n) {
+		int row = m;
+		int column = m + n;
+		int[][] matrix = new int[row][column];
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < column; c++) {
+				if (r == c)
+					matrix[r][c] = 1;
+			}
+		}
+		Relation resultRelation = new Relation();
+		resultRelation.setMatrix(matrix);
+		return resultRelation;
+	}
+
+	/**
+	 * This method create a relation with 1 and 0. The relation dimension
+	 * depends on the input parameters. For example, if m and n two parameters in
+	 * the function then relation consist of (n) rows and (m+n) columns. The result
+	 * relation contains 1 for those indexes where row and column represents same
+	 * element from n and 0 elsewhere.
+	 * 
+	 * @param m the first parameter.
+	 * @param n the second parameter.
+	 * @return the kappa relation consist of 1 and 0.
+	 */
+	public static Relation Kappa(int m, int n) {
+		int row = n;
+		int column = m + n;
+		int[][] matrix = new int[row][column];
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < column; c++) {
+				if (c == r + n - 1)
+					matrix[r][c] = 1;
+			}
+		}
+		Relation resultRelation = new Relation();
+		resultRelation.setMatrix(matrix);
+		return resultRelation;
+	}
+
+	/**
+	 * This method create a relation with 1 and 0. The relation dimension
+	 * depends on the input parameters. For example, if m and n two parameters in
+	 * the function then relation consist of (m*n) rows and (m) columns. The result
+	 * relation contains 1 for those indexes which represents the row element and 0 elsewhere.
+	 * 
+	 * @param m the first parameter.
+	 * @param n the second parameter.
+	 * @return the pi relation consist of 1 and 0.
+	 */
+	public static Relation Pi(int m, int n) {
+		int row = m * n;
+		int column = m;
+		int[][] matrix = new int[row][column];
+
+		int column_count = 0;
+		int row_count = -1;
+		for (int r = 0; r < row; r++) {
+			row_count++;
+			for (int c = 0; c < column; c++) {
+				if (c == column_count)
+					matrix[r][column_count] = 1;
+			}
+			if (row_count == n - 1) {
+				column_count++;
+				row_count = -1;
+			}
+		}
+		Relation resultRelation = new Relation();
+		resultRelation.setMatrix(matrix);
+		return resultRelation;
+	}
+
+
+	/**
+	 * This method create a relation with 1 and 0. The relation dimension
+	 * depends on the input parameters. For example, if m and n two parameters in
+	 * the function then relation consist of (m*n) rows and (n) columns. The result
+	 * relation contains 1 for those indexes which represents the row element and 0 elsewhere.
+	 * 
+	 * @param m the first parameter.
+	 * @param n the second parameter.
+	 * @return the rho relation consist of 1 and 0.
+	 */
+	public static Relation Rho(int m, int n) {
+		int row = m * n;
+		int column = n;
+		int[][] matrix = new int[row][column];
+
+		int column_count = 0;
+		int row_count = -1;
+		for (int r = 0; r < row; r++) {
+			row_count++;
+			for (int c = 0; c < column; c++) {
+				if (c == column_count)
+					matrix[r][c] = 1;
+			}
+			if (row_count == n - 1) {
+				row_count = -1;
+				column_count = 0;
+			} else {
+				column_count++;
+			}
+
+		}
+		Relation resultRelation = new Relation();
+		resultRelation.setMatrix(matrix);
+		return resultRelation;
+	}
+
 }
+
--- ConceptGeneration/src/com/relation/utility/Utility.java
+++ ConceptGeneration/src/com/relation/utility/Utility.java
@@ -1,16 +1,55 @@
 package com.relation.utility;
 
-public class Utility {
-	public static void PrintArray(boolean[][] a) {
-		int x = a.length;
-		int y = a[0].length;
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				System.out.print(a[i][j] + "\t");
-			}
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
+/**
+ * 
+ * @author sajal
+ *
+ */
+public class Utility {
+	/**
+	 * This method prints a two dimensional array in the form of row and column
+	 * @param data is the input array to print.
+	 */
+	public static void PrintArray(int[][] data) {
+		int row = data.length;
+		int column = data[0].length;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.print(data[i][j] + "\t");
+			}
 			System.out.println("\n");
 		}
 	}
+	
 
+	public static void Write(int[][] data) {
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("output.txt", "UTF-8");
+			
+			
+			int row = data.length;
+			int column = data[0].length;
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < column; j++) {
+					writer.print(String.valueOf(data[i][j])+" ");
+				}
+				writer.println();
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
+
+
